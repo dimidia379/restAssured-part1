35bff18904f9084dfc1db50b6308331404bb215c
dimidia379 <j.zvereva050@gmail.com>
2021-12-28 18:30:07 +0300

    tests added

---
+++ src/test/java/guru/qa/ReqresTests.java
@@ -0,0 +1,72 @@
+package guru.qa;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
+    @Test
+    void gettingUser() {
+        given()
+                .when()
+                .get("api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2));
+    }
+
+    @Test
+    void userNotFound() {
+        given()
+                .when()
+                .get("api/users/53")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void deleteUser() {
+        given()
+                .when()
+                .delete("api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void createPerson() {
+        String data = "{ \"name\": \"Who\", \"job\": \"doctor\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("api/users")
+                .then()
+                .statusCode(201)
+                .body("job", is("doctor"))
+                .body("name", is("Who"));
+    }
+
+    @Test
+    void updatePerson() {
+        String data = "{ \"name\": \"House\", \"job\": \"doctor\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .put("api/users/3")
+                .then()
+                .statusCode(200)
+                .body("job", is("doctor"))
+                .body("name", is("House"));
+    }
+}
